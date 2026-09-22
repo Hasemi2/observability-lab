14bce9374d702559eecd7bae9a187ba5d1964464
Hasemi2 <h171754@gmail.com>
2026-09-22 10:41:42 +0900

    first commit

---
+++ src/test/java/com/practice/observability_lab/ObservabilityLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.observability_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservabilityLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
